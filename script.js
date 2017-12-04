183e9f671a14cb44f186f1e7790e38cc0f42a295
Robert Harężlak <harezlak.robert7@gmail.com>
2017-12-05 00:34:43 +0100

    code refactor

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.querySelector('#gradient');
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = body.style.background + ";";
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = `${body.style.background};`;
 }
 
-color1.addEventListener("input", setGradient);
+_linearGradient = () => {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
 
-color2.addEventListener("input", setGradient);
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
